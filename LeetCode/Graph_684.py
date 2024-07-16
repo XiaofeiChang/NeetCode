afbcfa350d8a65b2655a9e496f12f19950bfd5fd
XiaofeiChang <953702362@qq.com>
2024-07-16 17:00:22 +0800

    solved Graph_684

--- LeetCode/Graph_684.py
+++ LeetCode/Graph_684.py
@@ -1,55 +1,8 @@
 """
-In this problem, a tree is an undirected graph that is connected and has no cycles.
 
-You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
-
-Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+METHOD 1::::::::::
 
 
-
-Example 1:
-
-
-Input: edges = [[1,2],[1,3],[2,3]]
-Output: [2,3]
-
-
-
-
-Example 2:
-
-
-Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
-Output: [1,4]
-
-
-
- Example 3:
-
-
-Input: edges = [[3,4],[1,2],[2,4],[3,5],[2,5]]
-Output: [2,5]
-
-
-
-Constraints:
-
-n == edges.length
-3 <= n <= 1000
-edges[i].length == 2
-1 <= ai < bi <= edges.length
-ai != bi
-There are no repeated edges.
-The given graph is connected.
-"""
-
-
-
-
-
-
-
-"""
         This method is not work, this rule is designed by observising the given cases, but it can not deal with::
         **
         Input edges =
@@ -60,6 +13,12 @@ The given graph is connected.
         **
         Expected
         [2,5]
+
+
+
+
+
+
 
 
 
@@ -87,19 +46,25 @@ The given graph is connected.
         """
 
 
-class Solution:
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
 
-        # # Dictionary to keep track of the parent of each node
+"""
+
+METHOD 2::::::::::
+BY CHATGPT
+
+
+
+
+# # Dictionary to keep track of the parent of each node
         # parent = {}
-
+        
         # # Helper function to find the root of a node with path compression
         # def find(x):
         #     if parent[x] != x:
         #         parent[x] = find(parent[x])  # Path compression
-
+                
         #     return parent[x]
-
+        
         # # Helper function to union two nodes
         # def union(x, y):
         #     rootX = find(x)
@@ -109,7 +74,7 @@ class Solution:
         #         return True
         #     print(parent)
         #     return False  # Return False if x and y are already connected
-
+        
         # # Process each edge in the graph
         # for u, v in edges:
         #     if u not in parent:
@@ -119,44 +84,164 @@ class Solution:
         #     if union(u, v) is False:
         #         return [u, v]  # Return the edge if it forms a cycle
 
+
+    
+        # def update_min_root(node_temp):
+        #     print("node_temp", node_temp)
+        #     # The current smallest connected node must equals itself
+        #     if parent[node_temp] == node_temp:
+        #         return node_temp
+        #     parent[node_temp] = update_min_root(parent[node_temp])
+        #     print("parent", parent)
+"""
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+
+
+        # find the **values in Dict** that are related to the node_temp
+        def update_traversal_root(node_temp):
+            update_key_node_list.append(node_temp)
+            # The current smallest connected node must equals itself
+            if parent[node_temp] != node_temp:
+                update_traversal_root(parent[node_temp])
+            return
+
+        # find the **keys in dict** that are related to the node_temp
+        def update_greater_traversal_root(node_temp):
+            for key, val in parent.items():
+                if val==node_temp or val==parent[node_temp]:
+                    update_key_node_list.append(key)
+            return
+
+
+        """ N.B. The key idea to use both update_traversal_root() && and update_greater_traversal_root() is based on the constraints:
+                *    edges[i] = [ai, bi]
+                *    1 <= ai < bi <= edges.length
+                *    ai != bi
+
+            In 'parent' dictionary, we use the 'key' to represent the current node
+                          use the 'value' to represent the smallest node that the current node could reach, named 'root' 
+
+
+            As ai < bi, our 'value' <= 'key'
+
+
+            We need to find both the keys equal to the current node, or the values equals to the current node, to update all nodes on the traversal.
+
+        """
+
+
+
         parent = {}
 
         for e_pair in edges:
+            # print(e_pair)
             node1 = e_pair[0]
             node2 = e_pair[1]
 
             if_node1 = (node1 in parent)
             if_node2 = (node2 in parent)
 
-            if if_node1 == False and if_node2 == False:
-                print("1")
+            # Case 1: ______________________________________________________________________
+            if if_node1==False and if_node2==False:
+                # print("Case 1")
                 min_root = node1
                 parent[node1] = min_root
                 parent[node2] = min_root
 
-            elif if_node1 == True and if_node2 == False:
-                print("2")
+
+            # Case 2: ______________________________________________________________________
+            elif if_node1==True and if_node2==False:
+                # print("Case 2")
                 min_root = parent[node1]
                 parent[node2] = min_root
 
-            elif if_node1 == False and if_node2 == True:
-                print("3")
-                min_root = min(node1, parent[node2])
-                parent[node1] = min_root
-                parent[node2] = min_root
+            # Case 3: ______________________________________________________________________
+            elif if_node1==False and if_node2==True:
 
-            elif if_node1 == True and if_node2 == True:
-                print("4")
-                # check if the current min roots are all 1
-                len_parent = len(parent)
-                sum_min_root = sum(parent.values())
-                if len_parent == sum_min_root:
-                    return e_pair
-                else:
-                    min_root = min(parent[node1], parent[node2])
+                update_key_node_list = []
+
+                if node1 < parent[node2]:
+                    min_root = node1
                     parent[node1] = min_root
-                    parent[node2] = min_root
-            print(parent)
+
+                    update_traversal_root(node2)
+                    update_greater_traversal_root(node2)
+
+
+
+                else:
+                    min_root = parent[node2]
+                    parent[node1] = min_root
+
+                    update_traversal_root(node1)
+                    update_greater_traversal_root(node1)
+
+                # print("key node list", update_key_node_list)
+                for key_node in update_key_node_list:
+                    parent[key_node] = min_root
+
+
+            # Case 4: ______________________________________________________________________
+            elif if_node1==True and if_node2==True:
+                # print("4")
+
+
+                if parent[node1] == parent[node2]:
+                    return e_pair
+
+
+
+                else:
+                    # min_root = min(parent[node1], parent[node2])
+                    # parent[node1] = min_root
+                    # parent[node2] = min_root
+
+
+
+                    # if parent[node1] < parent[node2]:
+                    #     min_root = parent[node1]
+                    #     parent[node1] = min_root
+                    #     parent[node2] = min_root
+                    #     print(parent[node1], parent[node2])
+
+                    #     update_min_root(node1)
+                    #     update_min_root(node2)
+                    # else:
+                    #     min_root = parent[node2]
+                    #     parent[node1] = min_root
+                    #     parent[node2] = min_root
+
+                    #     update_min_root(node2)
+                    #     update_min_root(node1)
+
+                    update_key_node_list = []
+
+                    if parent[node1] < parent[node2]:
+                        min_root = parent[node1]
+
+                        update_traversal_root(node2)
+                        update_greater_traversal_root(node2)
+
+
+
+                    else:
+                        min_root = parent[node2]
+
+                        update_traversal_root(node1)
+                        update_greater_traversal_root(node1)
+
+                    # print("key node list", update_key_node_list)
+                    for key_node in update_key_node_list:
+                        parent[key_node] = min_root
+
+
+
+
+            # print(parent)
+            # print("___________________________________")
 
 
 
